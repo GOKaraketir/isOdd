5c6457a124e66229cce1c0defe80b321e7742cf5
Osman Karaketir <osmankaraketir@gmail.com>
2022-06-02 23:01:58 +0300

    no need to abs

--- isOdd.go
+++ isOdd.go
@@ -2,14 +2,6 @@ package isOdd
 
 import "golang.org/x/exp/constraints"
 
-func abs[T constraints.Integer](val T) T {
-	if val < 0 {
-		return -val
-	} else {
-		return val
-	}
-}
-
 func IsOdd[T constraints.Integer](val T) bool {
-	return abs(val)%2 == 1
+	return val%2 == 1
 }
