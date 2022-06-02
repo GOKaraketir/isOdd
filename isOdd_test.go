db0d678d1bd7e18776882756a7ea17140d93658e
Osman Karaketir <osmankaraketir@gmail.com>
2022-06-02 22:56:08 +0300

    Working for minimum integer, test added for min int and max int, closes #1

--- isOdd_test.go
+++ isOdd_test.go
@@ -2,14 +2,31 @@ package isOdd
 
 import "testing"
 
-func TestOdd(t *testing.T) {
+const MaxUint = ^uint(0)
+const MinUint = 0
+const MaxInt = int(MaxUint >> 1)
+const MinInt = -MaxInt - 1
+
+func TestOdd_1(t *testing.T) {
 	if !IsOdd(3) {
 		t.Fatal("Test Failed")
 	}
 }
 
-func TestNotOdd(t *testing.T) {
+func TestOdd_2(t *testing.T) {
+	if !IsOdd(MaxInt) {
+		t.Fatal("Test Failed")
+	}
+}
+
+func TestNotOdd_1(t *testing.T) {
 	if IsOdd(4) {
+		t.Fatal("Test Failed")
+	}
+}
+
+func TestNotOdd_2(t *testing.T) {
+	if IsOdd(MinInt) {
 		t.Fatal("Test Failed")
 	}
 }
